58899dfdd0370e0ea834b358a4b74b3eb6844db6
Parth Dhawan <pdhawan2001@gmail.com>
2022-04-17 23:59:09 +0530

    completed

--- index.ts
+++ index.ts
@@ -2,7 +2,26 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+interface Todo { // interface are used to define a structure of an object
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then(respone => {
-  console.log(respone.data);
+  const todo = respone.data as Todo;
+
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+  
+  logTodo(id, title, completed);
 });
 
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+    The Todo with ID: ${id}
+    Has a title of: ${title}
+    Is it finished? ${completed}
+  `);
+};
